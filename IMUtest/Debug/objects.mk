41b688702c0d612a83438012d2071470bb0ed258
Alec Brewer <ambrewe3@ncsu.edu>
2021-10-24 17:29:36 -0400

    Something

---
+++ IMUtest/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"C:/Users/alecb/Downloads/sail_1_50_00_00__all/sail_1_50_00_00/source/ti/sail/lib/ccs/msp432/sail.lib" -l"C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/display/lib/display.aem4f" -l"C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/lib/drivers_msp432p401x.aem4f" -l"C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/fatfs/lib/ccs/m4f/fatfs.a" -l"C:/ti/simplelink_msp432p4_sdk_3_40_01_02/kernel/tirtos/packages/ti/dpl/lib/dpl_msp432p401x.aem4f" $(GEN_CMDS__FLAG) -l"C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ccs/msp432p4xx_driverlib.lib" -llibc.a
+
