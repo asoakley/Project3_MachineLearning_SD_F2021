d9afab75d3083e7d1a7a3c8d58c73dc6f181f4d4
Austin <austinoakley1322@gmail.com>
2021-10-26 10:34:04 -0400

    Created initial project for AudioTest, building off MotorsTest

---
+++ AudioTest/src/main.c
@@ -0,0 +1,25 @@
+#include "msp.h"
+#include <gpio.h>
+#include <timers.h>
+#include <motors.h>
+
+
+/**
+ * main.c
+ */
+
+// Globals
+
+
+
+void main(void)
+{
+        Init_Ports();
+        Init_Timers();
+        MotorsSimple(LEFT_OFF, FORWARD, RIGHT_OFF, FORWARD);
+
+        while(1){
+
+            ;
+        }
+}
